3cf654aaf5721a0350c24594954ec5516946a21f
Kauê <kaweddd047@gmail.com>
2025-12-18 19:42:30 -0300

    Tentando resolver exercício pt1
    Não consegui desta vez.

--- aula13/repetiçoes1.js
+++ aula13/repetiçoes1.js
@@ -1,8 +1,18 @@
 var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
+
+
+/*
+var c = 1
 while (c <= 6) {
     console.log(`Passo ${c}`)
-     // c = c + 1
+    c++ // c = c + 1
 }
+*/
+
 
 /*
 console.log('Tudo bem?')
---
+++ desafios/ex016/script.js
@@ -0,0 +1,10 @@
+function contar() {
+    var ini = document.getElementById('ini')
+    var fim = document.getElementById('fim')
+    var pas = document.getElementById('passo')
+    res = document.getElementById('res')
+    while (ini != fim) {
+        var pas = pas + pas
+    }
+} 
+ 
